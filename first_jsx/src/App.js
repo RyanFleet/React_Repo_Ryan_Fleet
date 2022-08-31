99ce693e8d3abdff6bbd4cf1e3bb62a83218a979
Ryan Fleet <ryanfleeter24@gmail.com>
2022-08-31 18:36:45 -0500

    commit

---
+++ first_jsx/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <div>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the dogs</li>
+      </div>
+    </div>
+  );
+}
+
+      export default App;
