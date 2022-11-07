ce54e4a38ff8c6782e930d35c960b734e50b36b5
Ryan Fleet <ryanfleeter24@gmail.com>
2022-11-06 18:42:36 -0600

    Single linked List

---
+++ FrontsSLL/sll.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+class SLL {
+    constructor(){
+        this.head = null
+    }
+    addFront(val){
+        let new_node = new Node(val)
+        if(!this.head){
+            this.head = new_node
+            return this
+        }
+        new_node.next = this.head
+        this.head = new_node
+        return this
+    }
+    removeFront(){
+        if(this.head == null){
+            return this.head
+        }
+        let remNode = this.head // var holds node we want to delete
+        this.head = remNode.next // moves head to next node leaving old head stranded
+        remNode.next = null // set stranded node to null to unchain from list
+        return this.head
+    }
+    front(){
+        if(this.head == null){
+            return this.head
+        } else {
+            return this.head.value
+        }
+    }
+}
+
+list1 = new SLL()
+list1.addFront(18)
+list1.addFront(5)
+list1.addFront(73)
+console.log(list1)
+
+list1.removeFront()
+
+console.log(list1.front())
