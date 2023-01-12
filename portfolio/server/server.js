543341ce21b5e5974fbe0707d84705bbfd77ea52
Ryan Fleet <ryanfleeter24@gmail.com>
2023-01-12 13:17:44 -0600

    started portfolio and added to skate app

---
+++ portfolio/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const cors = require('cors')
+const cookieParser = require('cookie-parser');
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded( {extended: true} ));
+
+require('dotenv').config()
+// require('./routes/user.routes')(app)
+// require('./routes/video.routes')(app)
+require('./config/mongoose.config')
+    
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
