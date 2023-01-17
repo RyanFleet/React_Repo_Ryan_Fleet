17bdcc81349a492165f7393aa83aa15ebd7b5ac9
Ryan Fleet <ryanfleeter24@gmail.com>
2023-01-17 13:31:59 -0600

    started Plane Geometry

--- Portfolio-Project/portfolio/main.js
+++ Portfolio-Project/portfolio/main.js
@@ -2,55 +2,62 @@ import './style.css'
 
 import * as THREE from 'three';
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(80,window.innerWidth / window.innerHeight,0.5,1000)
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg')
-})
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth,window.innerHeight)
-// renderer.setClearColor( 0xffffff, 0);
-camera.position.setZ(30);
+let scene, camera, renderer, star;
 
-renderer.render(scene,camera)
+  scene = new THREE.Scene()
+  camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.5, 1000)
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+    antialias: true
+  })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  // renderer.setClearColor( 0xffffff, 0);
+  camera.position.setZ(30);
 
-// LIGHT
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(1,1000,-2)
+  renderer.render(scene, camera)
 
-const ambientLight = new THREE.AmbientLight(0xffffff)
-const lightHelper = new THREE.PointLightHelper(pointLight)
-scene.add(lightHelper)
+  // LIGHT
+  const pointLight = new THREE.PointLight(0xffffff)
+  pointLight.position.set(1, 1000, -2)
 
-scene.add(pointLight,ambientLight)
+  const ambientLight = new THREE.AmbientLight(0xffffff)
+  const lightHelper = new THREE.PointLightHelper(pointLight)
+  scene.add(lightHelper)
 
-const controls = new OrbitControls(camera,renderer.domElement);
+  scene.add(pointLight, ambientLight)
 
-// STARS
-function addStars(){
-  const theGeometry = new THREE.SphereGeometry(14,24,24);
-  const randomColor = THREE.MathUtils.randInt(0, 0xffffff)
-  const material = new THREE.MeshStandardMaterial({color: randomColor})
-  const star = new THREE.Mesh(theGeometry,material);
-  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(2000));
-  star.position.set(x,y,z);
-  scene.add(star)
-}
-Array(1000).fill().forEach(addStars)
+  const controls = new OrbitControls(camera, renderer.domElement);
 
-// MOVE CAMERA 
-const moveCamera = () => {
-  const t = document.body.getBoundingClientRect().top;
+  // STARS
+  function addStars() {
+    const theGeometry = new THREE.SphereGeometry(14, 24, 24);
+    const randomColor = THREE.MathUtils.randInt(0, 0xffffff)
+    const material = new THREE.MeshStandardMaterial({ color: randomColor })
+    star = new THREE.Mesh(theGeometry, material);
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(2000));
+    star.position.set(x, y, z);
+    scene.add(star)
+  }
+  Array(1000).fill().forEach(addStars)
 
-  camera.position.z = t * -0.05;
-  camera.position.x = t * -0.002;
-  camera.position.y = t * -0.01;
 
-}
-document.body.onscroll = moveCamera
+  // Plane
+    const planeGeo = new THREE.PlaneGeometry(10,10)
+    const planeMat = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide})
+    const plane = new THREE.Mesh(planeGeo, planeMat)
+    scene.add(plane)
 
+
+  // MOVE CAMERA 
+  const moveCamera = () => {
+    const t = document.body.getBoundingClientRect().top;
+    
+  }
+  document.body.onscroll = moveCamera
+  
 
 
 // LOOP RUNNING ANIMATIONS
@@ -58,7 +65,6 @@ const animation = () => {
   requestAnimationFrame(animation);
   controls.update();
 
-  renderer.render(scene,camera);
+  renderer.render(scene, camera);
 }
-
 animation()
