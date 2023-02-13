b9d7fec4415bc9b3362538ecf09945b50e3370fc
Ryan Fleet <ryanfleeter24@gmail.com>
2023-02-12 20:29:33 -0600

    deleted path

--- skate-app/server/server.js
+++ skate-app/server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 8000;
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
-const path = require('path')
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(cookieParser())
